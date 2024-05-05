48bd7b2304d641398032021e5bd4f46fe07fbc6a
Mohammad Farid Uzzaman <60000543+farid555@users.noreply.github.com>
2024-05-06 01:06:35 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         // integer types
         byte aSingleByte = 100; // -128 to 127
         short sSmallNumber = 20000; // -32,768 to 32,767
@@ -19,8 +21,130 @@ public class Main {
         // Characters
         char copyrightSymbol = '\u00A9';
 
+ */
+        /////////////////****************//////////////////////
 
-        System.out.printf("This is the copyright symbol:  " +  copyrightSymbol );
+         /*
+        String name = "Mohammad Farid Zaman";
+        String job = "software developer";
+        int age = 32;
+
+        System.out.printf(String.valueOf(name.length()));
+        System.out.printf(String.valueOf(name.isEmpty()));
+        System.out.printf(String.valueOf(name.toUpperCase()));
+        System.out.printf(String.format("Hello there! I am %s. I am a %s. I am %d years old.", name, job, age));
+        System.out.printf("Hello there! I am " + name + ". I am a " + job + ".");
+        System.out.printf("This is the copyright symbol:  " +  copyrightSymbol + name);
+*/
+        /////////////////****************//////////////////////
+/*
+
+
+        System.out.print("What is your name? \n");
+        String nameInput = scanner.nextLine();
+
+        System.out.printf("Hello %s!  How Old are you? \n" ,nameInput);
+        int ageInput = Integer.parseInt(scanner.nextLine());
+       // cleans up the input buffer
+
+
+        System.out.printf("I am %d years old. \n", ageInput);
+
+
+        System.out.print("What language you prefer for programming. \n");
+
+        String langInput = scanner.nextLine();
+        System.out.printf("I prefer %s for programming.", langInput);
+*/
+
+        //////////////////********> Conditional Statements <********//////////////////////
+        /*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number1:  ");
+        double number1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Enter the first number2:  ");
+        double number2 = scanner.nextDouble();
+        scanner.nextLine();
+
+
+        System.out.print("What operation do you want perform:  ");
+        String operation = scanner.nextLine();
+
+        if( operation.equals("sum")){
+            System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+        }else if( operation.equals("sub")){
+            System.out.printf("%f - %f = %f", number1, number2, number1 - number2);
+        }else if( operation.equals("div")){
+            if(number2 == 0){
+                System.out.print("Can not divide by zero");
+            }else{
+                System.out.printf("%f / %f = %f", number1, number2, number1 / number2);
+            }
+        }else if( operation.equals("multi")){
+            System.out.printf("%f * %f = %f", number1, number2, number1 * number2);
+        }else{
+            System.out.printf("%s is not a supported operation", operation);
+        }
+*/
+        //////////////////*******> Switch Cases <*********//////////////////////
+/*
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number1:  ");
+        double number1 = scanner.nextDouble();
+        scanner.nextLine();
+
+        System.out.print("Enter the first number2:  ");
+        double number2 = scanner.nextDouble();
+        scanner.nextLine();
+
+
+        System.out.print("What operation do you want perform:  ");
+        String operation = scanner.nextLine();
+
+        switch (operation){
+            case "sum":
+                System.out.printf("%f + %f = %f", number1, number2, number1 + number2);
+                break;
+            case "sub":
+                System.out.printf("%f - %f = %f", number1, number2, number1 - number2);
+                break;
+            case "multi":
+                System.out.printf("%f * %f = %f", number1, number2, number1 * number2);
+                break;
+            case "div":
+                if(number2 == 0){
+                    System.out.print("Can not divide by zero");
+                }else{
+                    System.out.printf("%f / %f = %f", number1, number2, number1 / number2);
+                }
+                break;
+            default:
+                System.out.printf("%s is not supported operation.", operation);
+        }
+
+        scanner.close();
+
+        */
+        //////////////////*******> Arrays <*********//////////////////////
+
+     char[] vowels = new char[5];
+
+     vowels[0] = 'a';
+     vowels[1] = 'e';
+     vowels[2] = 'i';
+     vowels[3] = 'o';
+     vowels[4] = 'u';
+
+        System.out.println(vowels[2]);
+        System.out.println(Arrays.toString(vowels));
+
+        char[] vow = {'a', 'e', 'i', 'o', 'u'};
+        System.out.println(Arrays.toString(vow));
+
     }
 }
 
